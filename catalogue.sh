f226fd133e3ec501d8b779c4dc82a65b5bbb3ad1
Pavani T <pavanit.0810@gmail.com>
2025-01-31 11:16:14 +0530

    Demo on Git repo

---
+++ catalogue.sh
@@ -0,0 +1,25 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+
+useradd roboshop
+
+rm -rf /app
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+npm install
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemstl start catalogue
+
+dnf install mongodb-mongosh -y
+
+mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
+
